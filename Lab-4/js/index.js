de912004f5c059826654ac442e6ef841f32544e9
Ser-Iojik <serega160202@mail.ru>
2022-10-22 01:10:33 +0300

    Fix burger-menu

--- Lab-4/js/index.js
+++ Lab-4/js/index.js
@@ -6,7 +6,6 @@ window.onload = function () {
     pageloadtimeDiv.append('Page load time is '+ loadTime +' milliseconds');
 }
 
-
 $(document).ready(function(){
     $('#header').load("header.html");
  });
@@ -14,21 +13,3 @@ $(document).ready(function(){
  $(document).ready(function(){
     $('#footer').load("footer.html");
  });
-
-//  $(document).ready(function(){
-//     $('#header-game').load("../header.html");
-//  });
-
-//  $(document).ready(function(){
-//     $('#footer-game').load("../footer.html");
-//  });
-
-
-
- $(document).ready(function() {
-	$('.menu-burger__header').click(function(){
-                $('.menu-burger__header').toggleClass('open-menu');
-                $('.header__nav').toggleClass('open-menu');
-                $('body').toggleClass('fixed-page');
-	});
-});
